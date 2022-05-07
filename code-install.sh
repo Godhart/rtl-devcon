d6130d777414ad90982ae2fb1a76731ebd26a810
Nikolay Gniteev <godhart@gmail.com>
2022-05-07 17:59:10 +0300

    Suggested list of VSCode extensions to work with RTL

---
+++ code-install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# On the host
+code --install-extension aaron-bond.better-comments
+code --install-extension CoenraadS.bracket-pair-colorizer-2
+code --install-extension inu1255.easy-snippet
+code --install-extension mikestead.dotenv
+code --install-extension mishkinf.goto-next-previous-member
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension usernamehw.errorlens
+code --install-extension zgudino.editorconfig-vscode-snippet
+
+# In the container
+code --install-extension bodil.file-browser
+code --install-extension C28N.c28n-vs
+code --install-extension casperstorm.directory-files
+code --install-extension christian-kohler.path-intellisense
+code --install-extension ctf0.macros
+code --install-extension eamodio.gitlens
+code --install-extension EditorConfig.EditorConfig
+code --install-extension ExodiusStudios.comment-anchors
+code --install-extension formulahendry.code-runner
+code --install-extension johnnywong.vscode-ts-whitespace-converter
+code --install-extension micnil.vscode-checkpoints
+code --install-extension mrmlnc.vscode-duplicate
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension ms-vscode.hexeditor
+code --install-extension ms-vscode.js-debug-companion
+code --install-extension rebornix.toggle
+code --install-extension redhat.vscode-yaml
+code --install-extension ryu1kn.partial-diff
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension teros-technology.teroshdl
+code --install-extension tomoki1207.pdf
+code --install-extension Tyriar.sort-lines
+code --install-extension wmaurer.change-case
+code --install-extension yatki.vscode-surround
